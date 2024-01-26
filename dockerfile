5826dd3608f9256001b8847c175910dfcc82ea7e
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-01-26 14:00:39 +0000

    Deleted Dockerfile primarily because I don't know Docker. Also changed the main.py file.

--- dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.10.13-slim-buster
-
-WORKDIR /app
-
-COPY . /app
-
-RUN pip install --no-cache-dir -r requirements.txt
-
-CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, Response
 import os
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def count_views():
     count = int(os.getenv("VIEW_COUNT", 0))
     os.environ["VIEW_COUNT"] = str(count + 1)
     return Response(f'<svg><text x="0" y="15">{count}</text></svg>', mimetype='image/svg+xml')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run()
